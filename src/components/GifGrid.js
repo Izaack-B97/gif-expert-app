e08a57021c7177e990526a1bb13f1043095dcc3d
Isaac Bustamante <izaack.97@gmail.com>
2020-12-07 11:43:46 -0700

    Primera a´pp en react

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,34 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { GifGridItem } from './GifGridItem';
-// import {getGifs } from '../helpers/getGifs';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-    
-    // const [images, setImages] = useState([]);
-    
-    // useEffect(() => {
-    //     getGifs(category)
-    //         .then(setImages);
-    // }, [category]);
-
-    const { loading } = useFetchGifs();
+        
+    const { data: images ,loading } = useFetchGifs(category);
     console.log(loading);
 
     return (
-        <div>
-            <h3>{ category }</h3>
-            { loading ? 'Cargando . . . ' : 'Data cargada' }
-        </div>
-        // <Fragment>
-        //     <h3>{ category }</h3>
-        //     {/* <div className="card-grid">
-        //         {
-        //             images.map(image => {
-        //                 return <GifGridItem key={ image.id }{ ...image }/>
-        //             })
-        //         }
-        //     </div> */}
-        // </Fragment>
+       
+        <Fragment>
+            <h3 className="animate__animated animate__fadeIn">{ category }</h3>
+            { loading && <p className="animate__animated animate__flash">Loading . . .</p> }
+            <div className="card-grid">
+                {
+                    images.map(image => {
+                        return <GifGridItem key={ image.id }{ ...image }/>
+                    })
+                }
+            </div>
+        </Fragment>
     )
 }
