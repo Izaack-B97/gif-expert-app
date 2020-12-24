63983a6aad6980499026173195bcde8fce14f83e
Isaac Bustamante <izaack.97@gmail.com>
2020-12-24 14:17:32 -0700

    More tests

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,15 +3,39 @@ import { GifGridItem } from "../../components/GifGridItem";
 
 describe('Pruebas en <GifGridItem /> ' , () => {
     
+    const title = 'Mi titulo';
+    const url = 'https://example.com';
+
+    const wrapper = shallow(<GifGridItem title={ title } url={ url }/>);
+
     test('debe de mostrar correctamente el componente ', () => {
-
-        const title = 'Mi titulo';
-        const url = 'https://example.com';
-
-        const wrapper = shallow(<GifGridItem title={ title } url={ url }/>);
         
         expect( wrapper ).toMatchSnapshot();
 
     });
     
+    test('debe de tener un parrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect( p.text() ).toBe( title );
+    });
+
+    test('debe de tener la imagen igual a la url y alt de los props ', () => {
+        const img = wrapper.find('img');
+
+        // console.log(img.props()) // { src: 'https://example.com', alt: 'Mi titulo' } -> props devuelte las propiedas en enzyme
+
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title );
+    });
+    
+    test('debe de tener la clase animate__fadeIn" ', () => {
+        const clase = 'animate__fadeIn';
+        const div = wrapper.find('div');
+        const divClass = div.prop('className');
+
+        // console.log( div.props());
+        expect( divClass.includes( clase ) ).toBe( true );        
+    });
+    
+
 });
